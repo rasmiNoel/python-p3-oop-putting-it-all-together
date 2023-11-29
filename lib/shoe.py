d2464dc7aee05e828d30e13aed1df6fc86778112
rasmi <rasminoel@gmail.com>
2023-11-29 15:05:23 +0300

    Attempt 1 at Shoe

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand="", size=""):
+        self.brand = brand
+        self.size = size
+
+    def size(self):
+        if isinstance(self.size, int):
+            return self.size
+        else:
+            print("size must be an integer.")
+            
+    def repair(self, condition="New"):
+        self.condition = condition
